33fdf614d79a07b5f67f1ed2747aca48b6b36b83
Priyanshu7129 <priyanshu7129@gmail.com>
2025-03-02 20:36:38 +0530

    Implemented UC2 - Simple Login

---
+++ src/main/Java/com/firstServlet/LoginServlet.java
@@ -0,0 +1,28 @@
+package com.firstServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ("admin".equals(username) && "password".equals(password)) {
+            response.sendRedirect("loginSuccess.jsp");
+        } else {
+            response.getWriter().println("Invalid credentials");
+        }
+    }
+
+    // This handles GET requests
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Use the login form to submit credentials.");
+    }
+}
---
+++ src/main/webapp/loginSuccess.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" %>
+<html>
+<head>
+    <title>Login Success</title>
+</head>
+<body>
+    <h1>Login Successful!</h1>
+</body>
+</html>
